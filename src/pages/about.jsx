21920b5066690f8509d92598d2381e32a11b856b
Anwasi Ayebaemi <119889433+Ayewealth@users.noreply.github.com>
2023-07-01 14:45:54 +0100

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
 import "./CSS/about.css";
 
-import Header from "../components/home_components/header"
-import Footer from "../components/home_components/footer"
-import Banner from "../components/about_components/Banner"
+import Header from "../components/home_components/header";
+import Footer from "../components/home_components/footer";
+import Banner from "../components/about_components/Banner";
 import BannerHeading from "../components/about_components/BannerHeading";
 import AboutBody from "../components/about_components/AboutBody";
 import Team from "../components/home_components/team";
